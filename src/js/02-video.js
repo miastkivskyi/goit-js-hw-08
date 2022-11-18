296dc49382456363bd84bac46b509d38aed1c9ad
Rodion Myastkowskuy <rodion.myastkowskuy@gmail.com>
2022-11-18 13:23:12 +0200

    vimeoPlayer

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const vimeoPlayer = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+vimeoPlayer.on('play', function (evt) {
+  // evt.preventDefault();
+});
+vimeoPlayer.on('timeupdate', throttle(isTimeUpdate, 1000));
+timeLocal();
+
+function isTimeUpdate(evt) {
+  let time = evt.seconds;
+  const JSONTime = JSON.stringify(time);
+  localStorage.setItem(LOCALSTORAGE_KEY, JSONTime);
+  console.log('Сurrent playback time:', time);
+}
+
+function timeLocal() {
+  const savedTime = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (savedTime) {
+    vimeoPlayer.setCurrentTime(savedTime);
+  }
+}
