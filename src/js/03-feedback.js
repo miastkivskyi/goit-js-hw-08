c451515cdb68765267e1ad1205ec580c14b31588
Rodion Myastkowskuy <rodion.myastkowskuy@gmail.com>
2022-11-19 12:13:18 +0200

    update feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,23 +2,24 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
-const formData = {};
 
 form.addEventListener('submit', saveMessage);
 form.addEventListener('input', throttle(onTextareaInput, 500));
 messageLocal();
-
-function onTextareaInput(evt) {
-  formData[evt.target.name] = evt.target.value;
-  const JSONformData = JSON.stringify(formData);
-  localStorage.setItem(LOCALSTORAGE_KEY, JSONformData);
-}
 
 function saveMessage(evt) {
   evt.preventDefault();
   console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
   evt.currentTarget.reset();
   localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function onTextareaInput(evt) {
+  let formData = localStorage.getItem(LOCALSTORAGE_KEY);
+  formData = formData ? JSON.parse(formData) : {};
+  formData[evt.target.name] = evt.target.value;
+  const JSONformData = JSON.stringify(formData);
+  localStorage.setItem(LOCALSTORAGE_KEY, JSONformData);
 }
 
 function messageLocal() {
